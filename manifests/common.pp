25888d7148ec53e1ca3f51121fdee7a9f1111907
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-06-16 18:18:28 +0200

    Reorganize the manifests / classes (puppet 3)

--- manifests/common.pp
+++ manifests/common.pp
@@ -4,68 +4,6 @@
 # License::   GPLv3
 #
 # ------------------------------------------------------------------------------
-# = Class: pxelinux
-#
-# Configure and manage pxelinux
-#
-# == Parameters:
-#
-# $ensure:: *Default*: 'present'. Ensure the presence (or absence) of pxelinux
-# $source:: *Default*: empty. Source file for the default config file
-# $root_dir:: *Default*: '/srv/tftp'. Root directory for your tftp server
-#
-# == Actions:
-#
-# Install and configure pxelinux
-#
-# == Requires:
-#
-# n/a
-#
-# == Sample Usage:
-#
-#     import pxelinux
-#
-# You can then specialize the various aspects of the configuration,
-# for instance:
-#
-#         class { 'pxelinux':
-#             ensure    => present,
-#             source    => 'puppet:///modules/pxelinux/viridis-default',
-#             root_dir  => '/srv/tftp'
-#         }
-#
-# == Warnings
-#
-# /!\ Always respect the style guide available
-# here[http://docs.puppetlabs.com/guides/style_guide]
-#
-#
-# [Remember: No empty lines between comments and class definition]
-#
-class pxelinux(
-    $ensure    = $pxelinux::params::ensure,
-    $root_dir  = $pxelinux::params::root_dir,
-    $source    = ''
-)
-inherits pxelinux::params
-{
-    info ("Configuring pxelinux (with ensure = ${ensure})")
-
-    if ! ($ensure in [ 'present', 'absent' ]) {
-        fail("pxelinux 'ensure' parameter must be set to either 'absent' or 'present'")
-    }
-
-    case $::operatingsystem {
-        debian, ubuntu:         { include pxelinux::debian }
-        redhat, fedora, centos: { include pxelinux::redhat }
-        default: {
-            fail("Module $module_name is not supported on $operatingsystem")
-        }
-    }
-}
-
-# ------------------------------------------------------------------------------
 # = Class: pxelinux::common
 #
 # Base class to be inherited by the other pxelinux classes
@@ -121,19 +59,3 @@ class pxelinux::common {
     }
 
 }
-
-
-# ------------------------------------------------------------------------------
-# = Class: pxelinux::debian
-#
-# Specialization class for Debian systems
-class pxelinux::debian inherits pxelinux::common { }
-
-# ------------------------------------------------------------------------------
-# = Class: pxelinux::redhat
-#
-# Specialization class for Redhat systems
-class pxelinux::redhat inherits pxelinux::common { }
-
-
-
---
+++ manifests/common/debian.pp
@@ -0,0 +1,10 @@
+# File::      <tt>debian.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2013 Hyacinthe Cartiaux
+# License::   GPLv3
+#
+# ------------------------------------------------------------------------------
+# = Class: pxelinux::debian
+#
+# Specialization class for Debian systems
+class pxelinux::common::debian inherits pxelinux::common { }
